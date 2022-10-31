38a806cb92ab836cff38efece7a9ec3529905d0a
tomohiro-iwa <tomo1996722@gmail.com>
2022-10-31 18:39:12 +0900

    hsvで緑の領域だけ取得出来るようにした

--- PicAnalyzer.py
+++ PicAnalyzer.py
@@ -14,11 +14,11 @@ img = cv2.imread('./test.png')
 
 ## 緑だけを抽出した例 しきい値 125以上の場合だけ黒にした画像
 _, ret = cv2.threshold(img[:,:, GREEN], 125, 255, cv2.THRESH_BINARY)
-imshow("緑色がしきい値以上の部分", ret)
+#imshow("緑色がしきい値以上の部分", ret)
 _, ret = cv2.threshold(img[:,:, BLUE], 125, 255, cv2.THRESH_BINARY)
-imshow("blue色がしきい値以上の部分", ret)
+#imshow("blue色がしきい値以上の部分", ret)
 _, ret = cv2.threshold(img[:,:, RED], 125, 255, cv2.THRESH_BINARY)
-imshow("red色がしきい値以上の部分", ret)
+#imshow("red色がしきい値以上の部分", ret)
 
 redimg = img[:,:, RED]
 greenimg = img[:,:, GREEN]
@@ -34,5 +34,14 @@ result = greenimg - other
 #imshow("緑から青を引いた画像", greenimg - blueimg)
 #imshow("緑から赤を引いた画像", greenimg - redimg)
 
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+# 緑色のHSVの値域1
+hsv_min = np.array([30, 30, 0])
+hsv_max = np.array([90,255,255])
 
+# 緑色領域のマスク（255：赤色、0：赤色以外）    
+mask = cv2.inRange(hsv, hsv_min, hsv_max)
 
+masked_img = cv2.bitwise_and(img, img, mask=mask)
+
+imshow("hsv", masked_img)
